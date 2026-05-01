cf30d3c4dcc4092000d3ae8ebeda38d676d44fc5
f1rebeard <alxsrtv@gmail.com>
2026-05-01 11:59:44 +0300

    CH5 L5

--- main.go
+++ main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"bootdev-chirpy/internal/database"
+	"context"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	"sync/atomic"
+	"time"
 	"unicode/utf8"
+
+	"github.com/google/uuid"
 )
 import _ "github.com/lib/pq"
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db             *database.Queries
+	platform       string
+}
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -29,14 +45,61 @@ func (cfg *apiConfig) metricHits(w http.ResponseWriter, req *http.Request) {
 }
 
 func (cfg *apiConfig) metricHitsReset(w http.ResponseWriter, req *http.Request) {
-	cfg.fileserverHits.Store(0)
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	if cfg.platform != "dev" {
+		w.WriteHeader(http.StatusForbidden)
+		w.Write([]byte("You are using an incorrect platform"))
+		return
+	}
+	err := cfg.db.DeleteUsers(context.Background())
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	cfg.fileserverHits.Store(0)
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Fileserver hits reset to 0"))
 }
 
+func (cfg *apiConfig) createUserHandler(w http.ResponseWriter, r *http.Request) {
+	type email struct {
+		Email string `json:"email"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	newEmail := email{}
+	err := decoder.Decode(&newEmail)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	newUser, err := cfg.db.CreateUser(r.Context(), newEmail.Email)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not create user")
+		return
+	}
+	newUserResp := User{
+		ID:        newUser.ID,
+		CreatedAt: newUser.CreatedAt,
+		UpdatedAt: newUser.UpdatedAt,
+		Email:     newUser.Email,
+	}
+	respondWithJSON(w, http.StatusCreated, newUserResp)
+}
+
 func main() {
-	cfg := &apiConfig{}
+
+	dbURL := os.Getenv("DB_URL")
+	platform := os.Getenv("PLATFORM")
+	databaseQueries, err := connectionDatabase(dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cfg := &apiConfig{
+		db:       databaseQueries,
+		platform: platform,
+	}
+
 	appHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
 	assetsHandler := http.StripPrefix("/app/assets/", http.FileServer(http.Dir("assets")))
 
@@ -47,6 +110,7 @@ func main() {
 	mux.HandleFunc("GET /admin/metrics", cfg.metricHits)
 	mux.HandleFunc("POST /admin/reset", cfg.metricHitsReset)
 	mux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
+	mux.HandleFunc("POST /api/users", cfg.createUserHandler)
 
 	httpServer := &http.Server{}
 	httpServer.Handler = mux
@@ -128,4 +192,19 @@ func cleanChirp(body string) string {
 		}
 	}
 	return strings.Join(splitWords, " ")
+}
+
+func connectionDatabase(dbURL string) (*database.Queries, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.PingContext(ctx); err != nil {
+		return nil, err
+	}
+	databaseQueries := database.New(db)
+	return databaseQueries, nil
 }
